957f9051ffd839bb7b8d094bbf7c573dba4fdb48
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-12-19 14:55:40 +0530

    Added some middlewares and utility files

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// To handle CORS related issues
+app.use(cors({
+  origin: process.env.ORIGIN,
+  credentials: true
+}))
+
+app.use(express.json({ limit: '20kb'})) // To define size of json which can expect
+app.use(express.urlencoded({ extended: true, limit: "16kb" })) // To deal with query parameters
+app.use(express.static("public")) // To handle static assets
+app.use(cookieParser()) // To set and get cookies from client
+
+
+export default app; 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@
 
 import dotenv from 'dotenv'
 import connectDB from "./db/connect.js";
+import app from './app.js';
 
 dotenv.config({
   path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+  const port = process.env.PORT || 3000
+  app.listen(port, () => {
+    console.log("Server running on", port);
+  })
+
+  app.get('/', (req, res) => {
+    res.send("Welcome to The APP!")
+  })
+})
+.catch((err) => {
+  console.log("DB connection failed!", err);
+})
